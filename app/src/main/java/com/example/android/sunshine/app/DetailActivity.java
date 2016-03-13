105602a86c045f941d3b58e997e2643642e3f622
thisdotrob <robstevenson000@gmail.com>
2016-03-13 18:32:46 +0000

    Refactor retrieval of EXTRA_MESSAGE from intent

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,10 +1,8 @@
 package com.example.android.sunshine.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,10 +13,6 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Intent intent = getIntent();
-        CharSequence text = intent.getCharSequenceExtra(MainFragment.EXTRA_MESSAGE);
-        TextView textView = (TextView)findViewById(R.id.textview_detail);
-        textView.setText(text);
     }
 
 }
--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -1,10 +1,12 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DetailFragment extends Fragment {
 
@@ -15,6 +17,12 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent intent = getActivity().getIntent();
+        CharSequence text = intent.getCharSequenceExtra(MainFragment.EXTRA_MESSAGE);
+        TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
+        textView.setText(text);
+        return rootView;
+
     }
 }
