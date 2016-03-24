eff8c6dd8645c3b6e4253ea098b06cd34aa24716
Rob Stevenson <robstevenson000@gmail.com>
2016-03-24 14:28:32 +0000

    Update preference summary on value change

--- app/src/main/java/com/example/android/sunshine/app/SettingsFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
@@ -8,18 +9,40 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+        bindPreferencesToSummaries();
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen()
+                .getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        bindPreferencesToSummaries();
+    }
+
+    private void bindPreferencesToSummaries() {
         PreferenceScreen preferenceScreen = getPreferenceScreen();
         int preferenceCount = preferenceScreen.getPreferenceCount();
-
         for (int i = 0; i < preferenceCount; i++) {
             Preference preference = preferenceScreen.getPreference(i);
             bindPreferenceToSummary(preference);
@@ -27,7 +50,6 @@ public class SettingsFragment extends PreferenceFragment {
     }
 
     private void bindPreferenceToSummary(Preference preference) {
-
         if (preference instanceof PreferenceCategory) {
             PreferenceCategory category = (PreferenceCategory) preference;
             for (int i = 0; i < category.getPreferenceCount(); i++) {
@@ -48,5 +70,4 @@ public class SettingsFragment extends PreferenceFragment {
             preference.setSummary(preferenceValue);
         }
     }
-
 }
